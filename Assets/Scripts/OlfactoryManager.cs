11417137b4dad2e8b0defc29f1a29ac7d5d3d1d3
Legi01 <leonard.lgt@gmail.com>
2025-07-15 17:57:50 +0200

    adding Stas scene and we have a working copy

--- Assets/Scripts/OlfactoryManager.cs
+++ Assets/Scripts/OlfactoryManager.cs
@@ -12,10 +12,4 @@ public class OlfactoryManager : MonoBehaviour
         // Send signal to Arduino
         Debug.Log("Trigger scent: " + scentType);
     }
-
-    public void StopScent(string scentType)
-    {
-        // Send signal to Arduino
-        Debug.Log("Stop scent: " + scentType);
-    }
 }
